129deec3919441cc5181091a48638eb909638d36
vagrant <git@github.com:aka47coder/typewriter.git>
2021-12-01 23:00:21 +0000

    first commit

---
+++ play.js
@@ -0,0 +1,29 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+
+  const conn = net.createConnection({
+    host: 'localhost', // IP address here
+    port :  50541// PORT number here,
+     
+});
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  //conn.on("event name", connect);
+
+//   conn.on("event name", () => {
+//     // code that does something
+//   });
+  conn.on("connect", () => {
+    // code that does something when the connection is first established
+    console.log("you ded cuz you idled")
+  });
+
+
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();

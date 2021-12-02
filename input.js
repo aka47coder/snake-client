0089390ff8f4a8ee22637e2fb97e2f1af076db29
vagrant <git@github.com:aka47coder/typewriter.git>
2021-12-02 02:10:15 +0000

    new

---
+++ input.js
@@ -0,0 +1,34 @@
+let connection;
+const setupInput = (conn) => {
+    connection = conn;
+ 
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    
+  
+  const handleUserInput = function (key) {
+    if (key==="w"){
+        conn.write("Move: up");
+    }
+    if (key==="a"){
+        conn.write("Move: left");
+    }
+    if (key==="s"){
+        conn.write("Move: down");
+    }
+    if (key==="d"){
+        conn.write("Move: right");
+    }
+    
+    if (key === '\u0003') {
+        process.exit();
+      }
+      
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+  
+};
+module.exports={setupInput,};

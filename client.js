5d595c6bfaf540eab5082756052dbadb8c55bf8f
vagrant <git@github.com:aka47coder/typewriter.git>
2021-12-02 00:28:01 +0000

    new

--- client.js
+++ client.js
@@ -20,7 +20,11 @@ const connect = function () {
       // code that does something when the connection is first established
      // console.log("you ded cuz you idled")
       console.log("Successfully connected to game server")
-      conn.write("Name: HAZ")
+      conn.write("Name: HAZ");
+      // conn.write("Move: up");
+      // conn.write("Move: down");
+      // conn.write("Move: left");
+      // conn.write("Move: right");
     });
   
   
